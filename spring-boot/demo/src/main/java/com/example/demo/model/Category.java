96e006d4be30d7967406adca9416ffeb9daeee98
alejo <alejandrobravo0329@gmail.com>
2025-03-18 14:10:18 -0500

    rename file

--- spring-boot/demo/src/main/java/com/example/demo/model/Category.java
+++ spring-boot/demo/src/main/java/com/example/demo/model/Category.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Category {
     
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
